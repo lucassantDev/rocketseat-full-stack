bb4b92611e717c0997edf4f5ce145e116475d8d4
Lucas Sant <lucasfdesantana095@gmail.com>
2025-07-29 17:16:49 -0300

    finalizando scripts

--- javascript/projeto-lista-de-compras/script.js
+++ javascript/projeto-lista-de-compras/script.js
@@ -1,11 +1,16 @@
 // capturando dados do html
 const botaoNovoItem = document.getElementById('adicionar-item');
 const secaoItens = document.querySelector('#secao-03');
+const removerItem = document.querySelector('.remover-item')
 
 // eventos e funções
 
 function limparCampo(elemento){
     return elemento.value = '';
+}
+
+function removendoItemEscolhido(elemento){
+    return elemento.remove();
 }
 
 // capturando o item depois do clique no botão adicionar item
@@ -14,34 +19,42 @@ botaoNovoItem.addEventListener('click', (event) => {
     
     // capturando o valor do input de item
     let item = document.getElementById('novo-item');
+    if(item.value === ''){
+        alert('Não é possível cadastrar o produto');
+    }else{
+        // criando div que irá conter o elemento e posteriormente irá aparecer no html
+        let espacoItem = document.createElement('div');
 
-    // criando div que irá conter o elemento e posteriormente irá aparecer no html
-    let espacoItem = document.createElement('div');
+        // criando o paragráfo, o checkbox e o icone de lixeira
+        let paragrafo = document.createElement('p');
+        let checkboxHTML = document.createElement('input');
+        checkboxHTML.setAttribute('type', 'checkbox'); // adicionando atributos para o input
+        let spanIconeLixeira = document.createElement('span');
 
-    // criando o paragráfo, o checkbox e o icone de lixeira
-    let paragrafo = document.createElement('p');
-    // let checkboxHTML = document.createElement('');
-    let spanIconeLixeira = document.createElement('span')
+        // informando o item informado, criando um texto e armazenando na variavel valorItem
+        let valorItem = document.createTextNode(`${item.value}`);
+        let iconeLixeira = document.createTextNode('🗑️');
 
-    // informando o item informado, criando um texto e armazenando na variavel valorItem
-    let valorItem = document.createTextNode(`${item.value}`);
-    let iconeLixeira = document.createTextNode('🗑️');
+        // adicionando o elemento filho no elemento pai
+        paragrafo.appendChild(valorItem);
+        spanIconeLixeira.appendChild(iconeLixeira);
+        spanIconeLixeira.classList.add('remover-item')
 
-    // adicionando o elemento filho no elemento pai
-    paragrafo.appendChild(valorItem);
-    spanIconeLixeira.appendChild(iconeLixeira);
-    spanIconeLixeira.classList.add('remover-item')
+        espacoItem.appendChild(checkboxHTML)
+        espacoItem.appendChild(paragrafo);
+        espacoItem.appendChild(spanIconeLixeira)
 
-    espacoItem.appendChild(paragrafo);
-    espacoItem.appendChild(spanIconeLixeira)
+        console.log(espacoItem)
 
-    console.log(espacoItem)
+        // adicionando o estilo na div criada
+        espacoItem.classList.add('item-informado')
 
-    // adicionando o estilo na div criada
-    espacoItem.classList.add('item-informado')
+        // adicionando a div no html!!!!
+        secaoItens.appendChild(espacoItem);
+        limparCampo(item);
+    }
 
-
-    // adicionando a div no html!!!!
-    secaoItens.appendChild(espacoItem);
-    limparCampo(item);
+    spanIconeLixeira.addEventListener('click', () => {
+        removendoItemEscolhido(espacoItem);
+    })
 })
